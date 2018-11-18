7835e40b64eb11c8694b85bc4002a1b017bb79eb
KrosanAvenger <44968897+KrosanAvenger@users.noreply.github.com>
2018-11-18 23:38:19 +1030

    draw on screen demo
    -Adriano Anzellotti

--- app/src/main/java/com/example/dino/meadessentials/HexagonButton.java
+++ app/src/main/java/com/example/dino/meadessentials/HexagonButton.java
@@ -4,12 +4,15 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 public class HexagonButton extends View {
 
     private Paint paintHexagon = new Paint();
+    private Path pathHexagon = new Path();
 
     public HexagonButton (Context context) {
         super(context);
@@ -29,6 +32,8 @@ public class HexagonButton extends View {
     private void init(AttributeSet attrs, int defStyle)
     {
         this.paintHexagon.setColor(Color.RED);
+        this.paintHexagon.setAntiAlias(true);
+        this.paintHexagon.setStyle(Paint.Style.STROKE);
 
     }
 
@@ -36,7 +41,32 @@ public class HexagonButton extends View {
     public void onDraw(Canvas canvas)
     {
         super.onDraw(canvas);
-        canvas.drawLine(0,0,getWidth(), getHeight(), this.paintHexagon);
+        canvas.drawPath(this.pathHexagon,this.paintHexagon);
     }
 
+    @Override
+    public boolean onTouchEvent(MotionEvent motionEvent)
+    {
+        float touchX = motionEvent.getX();
+        float touchY = motionEvent.getY();
+
+        try {
+            switch (motionEvent.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    this.pathHexagon.moveTo(touchX, touchY);
+                    break;
+                case MotionEvent.ACTION_UP:
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    this.pathHexagon.lineTo(touchX, touchY);
+                    break;
+            }
+        } catch (Exception e)
+        {
+            return false;
+        }
+
+        invalidate();
+        return true;
+    }
 }
--- app/src/main/java/com/example/dino/meadessentials/MainMenu.java
+++ app/src/main/java/com/example/dino/meadessentials/MainMenu.java
@@ -1,15 +1,39 @@
 package com.example.dino.meadessentials;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainMenu extends AppCompatActivity {
 
+   private final Context CURRENT_CONTEXT = this;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+        final Button btnTerstInstance = findViewById(R.id.btnTest);
+
+        btnTerstInstance.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                startNewActivity(TestScreen.class);            }
+        });
     }
 
-    //this is a comment
+    private void startNewActivity(Class classToStart)
+    {
+        try {
+            Intent newActivity = new Intent(this.CURRENT_CONTEXT,classToStart);
+            startActivity(newActivity);
+        } catch (Exception e){
+            Log.i(classToStart.getName(),"Failed to launch new Activity: " + e.getMessage());
+        }
+    }
 }
