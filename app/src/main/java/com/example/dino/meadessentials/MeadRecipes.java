47b80b66c41ef6ef1cf712f8854c58284d2ecee8
KrosanAvenger <44968897+KrosanAvenger@users.noreply.github.com>
2018-12-01 15:43:27 +1030

    draw on screen demo
    -Adriano Anzellotti

--- app/src/main/java/com/example/dino/meadessentials/MeadRecipes.java
+++ app/src/main/java/com/example/dino/meadessentials/MeadRecipes.java
@@ -7,7 +7,6 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toolbar;
 
 public class MeadRecipes extends AppCompatActivity {
 
