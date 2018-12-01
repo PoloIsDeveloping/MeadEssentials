33507322321b1e679f06b295e2ae56e1814c7729
KrosanAvenger <44968897+KrosanAvenger@users.noreply.github.com>
2018-12-01 15:35:18 +1030

    draw on screen demo
    -Adriano Anzellotti

--- app/src/main/java/com/example/dino/meadessentials/MeadRecipes.java
+++ app/src/main/java/com/example/dino/meadessentials/MeadRecipes.java
@@ -11,23 +11,24 @@ import android.widget.Toolbar;
 
 public class MeadRecipes extends AppCompatActivity {
 
-    Toolbar toolbar;
+    android.support.v7.widget.Toolbar toolbar;
     ListView listview;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mead_recipes);
-
-        this.toolbar = (Toolbar) findViewById(R.id.toolbar);
+        this.toolbar = findViewById(R.id.customToolbar);
         this.toolbar.setTitle(getResources().getString(R.string.app_name));
-        this.listview = (ListView) findViewById(R.id.listview);
+        this.listview = findViewById(R.id.customListView);
 
         ArrayAdapter<String> mAdapter = new ArrayAdapter<>(MeadRecipes.this,
                 android.R.layout.simple_list_item_1,
                 getResources().getStringArray(R.array.MeadRecipes));
 
-        this.listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        this.listview.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 Intent intent = new Intent(MeadRecipes.this, SeccondActivity.class);
